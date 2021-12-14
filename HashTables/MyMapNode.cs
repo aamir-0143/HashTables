2470d9db005ad8a9fdceea8dcdfaed6cac19b78a
Md aamir reza <aamirreza156@gamil.com>
2021-12-14 15:13:05 +0530

    [Aamir15] Add : Added FindTheFrequencyOfWords

---
+++ HashTables/MyMapNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTables 
+{
+    public class MyMapNode<K, V>
+    {
+        private readonly int size;
+        private readonly LinkedList<KeyValue<K, V>>[] map;
+        public MyMapNode(int size)
+        {
+            this.size = size;
+            this.map = new LinkedList<KeyValue<K, V>>[size];
+        }
+        protected int GetArrayPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = map[position];
+            foreach (KeyValue<K, V> map in LinkedList)
+            {
+                if (map.key.Equals(key))
+                {
+                    return map.value;
+                }
+            }
+            return default(V);
+        }
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> LinkedList = map[position];
+            if (LinkedList == null)
+            {
+                LinkedList = new LinkedList<KeyValue<K, V>>();
+                map[position] = LinkedList;
+
+            }
+            return LinkedList;
+        }
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> LinkedList = GetLinkedList(position);
+            KeyValue<K, V> map = new KeyValue<K, V>() { key = key, value = value };
+            LinkedList.AddLast(map);
+
+        }
+
+
+        public struct KeyValue<K, V>
+        {
+            public K key { get; set; }
+            public V value { get; set; }
+        }
+    }
+}
